a7ff2d9b1ae0b6f16b379ce907847bfcc2bd8c2b
Daniele Viti <dnviti@gmail.com>
2025-06-13 17:33:38 +0000

    fixed entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
-# Start the cron service in the background.
-cron
+# Start the cron service as root in the background.
+# This ensures it has permissions to write its PID file.
+/usr/sbin/cron -f &
 
-# Execute the original command (e.g., "apache2-foreground")
+# Execute the original command passed to the container (e.g., "apache2-foreground")
+# This command will run as the default 'www-data' user defined in the Dockerfile.
 exec "$@"
