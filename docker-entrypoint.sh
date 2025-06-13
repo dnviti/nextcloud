438ed3312bdde409d1134c06384bff28ab8282aa
Daniele Viti <dnviti@gmail.com>
2025-06-13 17:55:58 +0000

    -

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,11 @@
 set -e
 
 # Start the cron service as root in the background.
-# This ensures it has permissions to write its PID file.
 /usr/sbin/cron -f &
+
+# Change ownership of the apache log directory to the www-data user.
+# This ensures that Apache, when running as www-data, can write to its logs.
+chown -R www-data:www-data /var/log/apache2
 
 # Use gosu to drop from root to the non-privileged 'www-data' user
 # and then execute the main command passed to this script (e.g., "apache2-foreground").
